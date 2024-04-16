7a8aa8deb3ae15268f400be9d5b01daf9661cd40
Gravitana <v.a.grahov@yandex.ru>
2024-04-17 03:08:27 +0500

    buttons active

--- app/src/main/java/ru/gravitana/buscounter/MainActivity.kt
+++ app/src/main/java/ru/gravitana/buscounter/MainActivity.kt
@@ -1,20 +1,55 @@
 package ru.gravitana.buscounter
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import ru.gravitana.buscounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var counter = 0
+    val placesTotal = 50
+
+    @SuppressLint("StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.plusButton.setOnClickListener {
+            if (counter == placesTotal) {
+                binding.resetButton.visibility = View.VISIBLE
+                binding.messageTextView.setTextColor(getColorStateList(R.color.red))
+                binding.messageTextView.text = getText(R.string.too_many_passengers)
+            } else {
+                counter++
+                binding.messageTextView.setTextColor(getColorStateList(R.color.black))
+                binding.messageTextView.text = getString(R.string.places_left, placesTotal - counter)
+            }
+            binding.counterTextView.text = counter.toString()
+        }
+
+        binding.minusButton.setOnClickListener {
+            if (counter > 0) {
+                counter--
+
+                if (counter == 0) {
+                    binding.messageTextView.setTextColor(getColorStateList(R.color.green))
+                    binding.messageTextView.text = getText(R.string.all_seats_are_available)
+                } else {
+                    binding.messageTextView.setTextColor(getColorStateList(R.color.black))
+                    binding.messageTextView.text = getString(R.string.places_left, placesTotal - counter)
+                }
+            }
+            binding.counterTextView.text = counter.toString()
+        }
+
+        binding.resetButton.setOnClickListener {
+            counter = 0
+
+            binding.counterTextView.text = counter.toString()
+            binding.messageTextView.setTextColor(getColorStateList(R.color.green))
+            binding.messageTextView.text = getText(R.string.all_seats_are_available)
         }
     }
 }
